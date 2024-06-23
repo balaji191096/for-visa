53c9e31c59463bb20da6a504bfd4c76ceb45cc25
Balaji <balajivenkat@engineer.com>
2024-06-23 22:27:54 +0530

    build script added

--- server/src/index.js
+++ server/src/index.js
@@ -10,13 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
 app.get('/getFiles', async (req, res) => {
-    const imageFilesDirectory = path.join(__dirname, '../Banner/Login'); // Here you have to change the file directory name 
+    const imageFilesDirectory = path.join(__dirname, '../../Banner/Login'); 
 
     fs.readdir(imageFilesDirectory, (err, files) => {
         if (err)
-            res.json({ statusCode: 400, message: "Unable to fetch the directory" });
+            res.status(500).json({ statusCode: 500, message: "Unable to fetch the directory" });
 
         res.status(200).json({ message: "Success", data: files });
     });
